2d5adff3ad119c2644409a7d78783ac7092db78e
serr <sc7227484@gmail.com>
2026-02-22 16:17:38 +0300

    small changes

--- ciphers/des.h
+++ ciphers/des.h
@@ -4,19 +4,21 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-uint64_t des_dec(uint64_t block, uint64_t masterkey);
+uint64_t des_dec(uint64_t block, uint64_t masterkey, uint32_t rounds);
 
 #ifdef DES_IMPL
 
-#define ROUNDS 16 // in standard
-
-static void des_generate_round_keys(uint64_t masterkey, uint64_t *roundkeys) {
+static void des_generate_round_keys(uint64_t masterkey, uint64_t *roundkeys, uint32_t rounds) {
 
 }
 
-uint64_t des_dec(uint64_t block, uint64_t masterkey) {
-    uint64_t *roundkeys = (uint64_t *)malloc(ROUNDS*sizeof(uint64_t));
-    des_generate_round_keys(masterkey, roundkeys);
+uint64_t des_dec(uint64_t block, uint64_t masterkey, uint32_t rounds) {
+    if (rounds != 16) {
+        fprintf(stderr, "des need 16 rounds (standard)\n");
+        exit(1);
+    }
+    uint64_t *roundkeys = (uint64_t *)malloc(rounds*sizeof(uint64_t));
+    des_generate_round_keys(masterkey, roundkeys, rounds);
     
     free(roundkeys);
 }
